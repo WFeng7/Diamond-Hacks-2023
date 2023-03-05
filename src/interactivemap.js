4dc85cbd3ed8baf810b9c2186d003f15f325e993
PerseusPotter <danktha0s@gmail.com>
2023-03-05 13:59:50 -0500

    bs

--- src/interactivemap.js
+++ src/interactivemap.js
@@ -22,3 +22,31 @@ shelters.forEach(v => {
 homeless.forEach(v => {
     L.circleMarker(v, { radius: 1 }).addTo(map);
 });
+
+document.getElementById('addThing').addEventListener('click', () => {
+    let h = homeless.slice();
+    shelters.forEach(v => {
+        for (let i = h.length - 1; i >= 0; i--) {
+            let dist = (h[i][0] - v[0]) ** 2 + (h[i][1] - v[1]) ** 2;
+            if (dist < 0.01) h.splice(i, 1);
+        }
+    });
+    if (!h.length) alert('no more shelters needed!');
+    let best;
+    let bestC;
+    h.forEach(v => {
+        let c = 0;
+        for (let i = h.length - 1; i >= 0; i--) {
+            let dist = (h[i][0] - v[0]) ** 2 + (h[i][1] - v[1]) ** 2;
+            if (dist < 0.071824) c++;
+        }
+        if (!best || bestC < c) {
+            best = v;
+            bestC = c;
+        }
+    });
+    shelters.push(best);
+    console.log(best);
+    var marker = L.marker(best).addTo(map);
+    marker._icon.classList.add("huechange");
+});
