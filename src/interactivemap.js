fcec6d99e1dcb512a192838f426e9ab7be13afe9
PerseusPotter <danktha0s@gmail.com>
2023-03-05 13:45:31 -0500

    lmfao

--- src/interactivemap.js
+++ src/interactivemap.js
@@ -2,7 +2,7 @@ const toggleBtn = document.querySelector('.toggle_btn')
 const toggleBtnIcon = document.querySelector('.toggle_btn i')
 const dropDownMenu = document.querySelector('.dropdown_menu')
 
-toggleBtn.onclick = function () {
+toggleBtn.onclick = function() {
     dropDownMenu.classList.toggle('open')
     const isOpen = dropDownMenu.classList.contains('open')
 
@@ -10,3 +10,23 @@ toggleBtn.onclick = function () {
         ? 'fa-solid fa-xmark'
         : 'fa-solid fa-bars'
 }
+
+var map = L.map('map').setView([35.7596, -78.5], 10);
+map.dragging.disable();
+map.touchZoom.disable();
+map.doubleClickZoom.disable();
+map.scrollWheelZoom.disable();
+map.boxZoom.disable();
+map.keyboard.disable();
+if (map.tap) map.tap.disable();
+document.getElementById('map').style.cursor = 'default';
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+shelters.forEach(v => {
+    L.marker(v).addTo(map);
+});
+homeless.forEach(v => {
+    L.circleMarker(v, { radius: 1 }).addTo(map);
+});
